c2b50fea34059fee0b2af327efdc53fd65c2d4d0
sellaksh <47029809+sellaksh@users.noreply.github.com>
2019-01-29 17:15:19 +0530

    Add files via upload
    first comment

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace WorldBankWebClientApplication.Controllers
+{
+    public class CountriesController : Controller
+    {
+        // GET: Countries
+        public ActionResult List()
+        {
+            return View();
+        }
+        public async Task<ActionResult> List1()
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("http://api.worldbank.org/countries");
+            var data = await response.Content.ReadAsStringAsync();
+            XDocument doc = XDocument.Parse(data);
+            XNamespace ns = "http://www.worldbank.org";
+            var names = from n in doc.Descendants(ns + "country")
+                        select n.Element(ns + "name").Value;
+            return PartialView(names.ToList<string>());
+        }
+    }
+}
